cdf22540193c500cd692ee55242c9d29dd190fa8
github username <github email address>
2020-09-29 13:07:11 -0600

    add index and create in controller, has_secure_pass, routes for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_165917) do
+ActiveRecord::Schema.define(version: 2020_09_29_190202) do
 
   create_table "trees", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2020_09_28_165917) do
     t.integer "height_min"
     t.string "fun_fact"
     t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
